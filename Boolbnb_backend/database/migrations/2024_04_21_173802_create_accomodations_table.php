4cd7a32d22a02ab02702d4e433446f474ba4370d
Alessandro Campo <alessandrocampo97@gmail.com>
2024-04-21 20:12:20 +0200

    updated  accomodation_table migration

--- Boolbnb_backend/database/migrations/2024_04_21_173802_create_accomodations_table.php
+++ Boolbnb_backend/database/migrations/2024_04_21_173802_create_accomodations_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Ad;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('accomodations', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false)->max(255);
+            $table->string('type')->nullable(true)->max(255);
+            $table->tinyInteger('rooms')->nullable(false);
+            $table->tinyInteger('beds')->nullable(false)->default(1);
+            $table->tinyInteger('bathrooms')->nullable(true);
+            $table->decimal('square_m', total: 10, places: 2)->nullable(true);
+            $table->string('address')->nullable(false)->max(255);
+            $table->decimal('latitude', total: 10, places: 6)->nullable(false);
+            $table->decimal('longitude', total: 10, places: 6)->nullable(false);
+            $table->decimal('price_per_night', total: 10, places: 2)->nullable(false);
+            $table->boolean('hidden')->default(false);
+            $table->string('thumb')->max(255)->nullable(true);
+            $table->string('host_thumb')->max(255)->nullable(true);
+            $table->decimal('price_per_night', total: 10, places: 2)->nullable(false);
+            $table->decimal('rating', total: 3, places: 2)->nullable(true);
+            $table->foreignId(User::class)->nullable(true)->default(null);
             $table->timestamps();
         });
     }
