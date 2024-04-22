0d27d1e3873ea31bd69e69baddea127242d66561
Alessandro Campo <alessandrocampo97@gmail.com>
2024-04-22 18:00:09 +0200

    feat: api routes returns images and services

--- Boolbnb_backend/app/Http/Controllers/Api/AccomodationController.php
+++ Boolbnb_backend/app/Http/Controllers/Api/AccomodationController.php
@@ -11,12 +11,25 @@ class AccomodationController extends Controller
     public function index(Request $request)
     {
 
-
         $min_price = $request->query('min_price');
-        $max_price = $request->query('min_price');
+        $max_price = $request->query('max_price');
+        $type = $request->query('type');
 
-        $accomodations = Accomodation::paginate(15);
-        if ($accomodations->count() > 0) {
+        $query = Accomodation::query();
+
+
+        if ($min_price !== null && $max_price !== null) {
+            $query->whereBetween('price_per_night', [$min_price, $max_price]);
+        }
+
+        if ($type !== null) {
+            $query->where('type', $type);
+        }
+
+
+        $accomodations = $query->paginate(15);
+        $accomodations = $query->with(['pictures', 'services'])->paginate(15);
+        if ($accomodations->total() > 0) {
             return response()->json([
                 'success' => true,
                 'res' => $accomodations
