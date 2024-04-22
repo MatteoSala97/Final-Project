9734929bf82f725860b19b4ced60af36be478b6d
Matteo Sala <matteo111997@gmail.com>
2024-04-22 18:06:54 +0200

    waiting to merge with alex

--- Boolbnb_backend/routes/api.php
+++ Boolbnb_backend/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\AccomodationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\AccomodationController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +17,4 @@ use App\Http\Controllers\Api\AccomodationController;
 |
 */
 
-<<<<<<< HEAD
 Route::get('/accomodations', [AccomodationController::class, 'index']);
-=======
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/accommodations', [AccomodationController::class, 'index']);
->>>>>>> 0d27d1e3873ea31bd69e69baddea127242d66561
