72976d5e61d73dfd8a201ad4af6c4a6b04c689e7
Matteo Sala <matteo111997@gmail.com>
2024-04-26 11:26:45 +0200

    fix: fixed logo

--- Boolbnb_backend/resources/views/layouts/app.blade.php
+++ Boolbnb_backend/resources/views/layouts/app.blade.php
@@ -20,8 +20,6 @@
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
         @include('layouts.navigation')
-        ciaooooo
-
         <!-- Page Heading -->
         @if (isset($header))
             <header class="bg-white shadow">
