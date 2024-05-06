b80bd89c75e2c582e3e81c3d2ad51b402da869f4
Matteo Sala <matteo111997@gmail.com>
2024-05-06 15:35:09 +0200

    fixed: favicon

--- Boolbnb_backend/resources/views/layouts/app.blade.php
+++ Boolbnb_backend/resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Bool BnB Dashboard</title>
 
+    <link rel="shortcut icon" href="{{ asset('logo-colored.svg') }}" >
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
