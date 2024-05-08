e68fdaf425e967dad1e6f1dcce4f6c031ce250c0
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-08 15:22:56 +0200

    x

--- Boolbnb_backend/resources/views/pages/accomodation/messages.blade.php
+++ Boolbnb_backend/resources/views/pages/accomodation/messages.blade.php
@@ -5,80 +5,74 @@
         </h2>
     </x-slot>
 
-    <div class="h-full">
-        <h2 class="font-bold text-xl p-5 ml-4">Messages ({{ $messages->count() }})</h2>
-        <div class="subtitle flex justify-between m-5">
-        </div>
-        <div class="h-screen">
-            <h2 class="font-bold text-xl p-5 ml-4">Messages ({{ $messages->total() }})</h2>
-            <!-- Table responsive wrapper -->
-            <div class="tabella overflow-x-auto bg-white mx-4">
+    <div class="h-screen">
+        <h2 class="font-bold text-xl p-5 ml-4">Messages ({{ $messages->total() }})</h2>
+        <!-- Table responsive wrapper -->
+        <div class="tabella overflow-x-auto bg-white mx-4">
 
-                <!-- Table -->
-                <table class="min-w-full text-left text-sm whitespace-nowrap">
+            <!-- Table -->
+            <table class="min-w-full text-left text-sm whitespace-nowrap">
 
-                    <!-- Table head -->
-                    <thead class="uppercase tracking-wider border border-x-0">
-                        <tr>
-                            <th scope="col" class="px-6 py-5 th-id">
-                                Thumbnail Image or id
-                            </th>
-                            <th scope="col" class="px-6 py-5 th-title">
-                                Accommodation
-                            </th>
-                            <th scope="col" class="px-6 py-5 th-name">
-                                Sender Name
-                            </th>
-                            <th scope="col" class="px-6 py-5 th-content">
-                                Message
-                            </th>
-                            <th scope="col" class="px-6 py-5 th-created_at">
-                                Time and date
-                            </th>
-                        </tr>
-                    </thead>
+                <!-- Table head -->
+                <thead class="uppercase tracking-wider border border-x-0">
+                    <tr>
+                        <th scope="col" class="px-6 py-5 th-id">
+                            Thumbnail Image or id
+                        </th>
+                        <th scope="col" class="px-6 py-5 th-title">
+                            Accommodation
+                        </th>
+                        <th scope="col" class="px-6 py-5 th-name">
+                            Sender Name
+                        </th>
+                        <th scope="col" class="px-6 py-5 th-content">
+                            Message
+                        </th>
+                        <th scope="col" class="px-6 py-5 th-created_at">
+                            Time and date
+                        </th>
+                    </tr>
+                </thead>
 
-                    <!-- Table body -->
-                    <tbody>
-                        @foreach ($messages as $message)
-                            @if ($message->accomodation)
-                                <tr class="border border-x-0 hover:bg-neutral-100 message-row view-message-btn cursor-pointer"
-                                    data-message-id="{{ $message->id }}"
-                                    onclick="window.location='{{ route('messages.show', ['message' => $message->id]) }}';">
+                <!-- Table body -->
+                <tbody>
+                    @foreach ($messages as $message)
+                        @if ($message->accomodation)
+                            <tr class="border border-x-0 hover:bg-neutral-100 message-row view-message-btn cursor-pointer"
+                                data-message-id="{{ $message->id }}"
+                                onclick="window.location='{{ route('messages.show', ['message' => $message->id]) }}';">
 
-                                    <?php $accomodationFound = false; ?>
-                                    @foreach ($accomodations as $item)
-                                        @if ($item->id == $message->accomodation->id)
-                                            <?php $accomodationFound = true; ?>
-                                            <td scope="row" class="px-6 py-5 td-id">
-                                                @if ($item->thumb)
-                                                    <img src="{{ asset($item->thumb) }}"
-                                                        style="height: 80px; width: 110px;"
-                                                        class="{{ $item->hidden ? 'grayscale' : '' }}" id="old_thumb">
-                                                @else
-                                                    <span>
-                                                        {{ $item->id }}
-                                                    </span>
-                                                @endif
-                                            </td>
-                                        @break
-                                    @endif
-                                @endforeach
-
-                                @if (!$accomodationFound)
-                                    <td></td>
+                                <?php $accomodationFound = false; ?>
+                                @foreach ($accomodations as $item)
+                                    @if ($item->id == $message->accomodation->id)
+                                        <?php $accomodationFound = true; ?>
+                                        <td scope="row" class="px-6 py-5 td-id">
+                                            @if ($item->thumb)
+                                                <img src="{{ asset($item->thumb) }}" style="height: 80px; width: 110px;"
+                                                    class="{{ $item->hidden ? 'grayscale' : '' }}" id="old_thumb">
+                                            @else
+                                                <span>
+                                                    {{ $item->id }}
+                                                </span>
+                                            @endif
+                                        </td>
+                                    @break
                                 @endif
+                            @endforeach
 
-                                <td class="px-6 py-5 td-title">{{ $message->accomodation->title }}</td>
-                                <td class="px-6 py-5 td-name">{{ $message->name }}</td>
-                                <td class="px-6 py-5 td-content">{{ $message->content }}</td>
-                                <td class="px-6 py-5 td-created_at">{{ $message->created_at->format('Y-m-d') }}
-                                </td>
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-                {{-- <tbody>
+                            @if (!$accomodationFound)
+                                <td></td>
+                            @endif
+
+                            <td class="px-6 py-5 td-title">{{ $message->accomodation->title }}</td>
+                            <td class="px-6 py-5 td-name">{{ $message->name }}</td>
+                            <td class="px-6 py-5 td-content">{{ $message->content }}</td>
+                            <td class="px-6 py-5 td-created_at">{{ $message->created_at->format('Y-m-d') }}</td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
+            {{-- <tbody>
                     @foreach ($messages as $message)
                         @if ($message->accomodation)
                             <tr class="border-b hover:bg-neutral-100 message-row view-message-btn cursor-pointer"
@@ -106,45 +100,6 @@
                         @endif
                     @endforeach
                 </tbody> --}}
-
-                <tbody>
-                    @foreach ($messages as $message)
-                        @if ($message->accomodation)
-                            <tr class="border-b hover:bg-neutral-100 message-row view-message-btn cursor-pointer"
-                                data-message-id="{{ $message->id }}"
-                                onclick="window.location='{{ route('messages.show', ['message' => $message->id]) }}';">
-
-                                <?php $accomodationFound = false; ?>
-                                @foreach ($accomodations as $item)
-                                    @if ($item->id == $message->accomodation->id)
-                                        <?php $accomodationFound = true; ?>
-                                        <td scope="row" class="px-6 py-5 td-id" style="height: 80px">
-                                            @if ($item->thumb)
-                                                <img src="{{ asset($item->thumb) }}" style="height: 80px;"
-                                                    class="{{ $item->hidden ? 'grayscale' : '' }}" id="old_thumb">
-                                            @else
-                                                <span>
-                                                    {{ $item->id }}
-                                                </span>
-                                            @endif
-                                        </td>
-                                    @break
-                                @endif
-                            @endforeach
-
-                            @if (!$accomodationFound)
-                                <td></td>
-                            @endif
-
-                            <td class="px-6 py-5">{{ $message->accomodation->title }}</td>
-                            <td class="px-6 py-5">{{ $message->created_at }}</td>
-                            <td class="px-6 py-5">{{ $message->name }}</td>
-                            <td class="message-body px-6 py-5">{{ $message->content }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-
         </table>
         <div class="mt-5 mx-10">
             {{ $messages->links() }}
@@ -173,31 +128,147 @@
 
 
 <style>
+.th-id,
+.td-id,
+.th-title,
+.td-title,
+.th-name,
+.td-name,
+.th-content,
+.td-content,
+.th-created_at,
+.td-created_at {
+    max-width: 200px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+
+
+@media screen and (max-width: 1240px) {
+
+    .th-content,
+    .td-content,
+    .th-title,
+    .td-title {
+        max-width: 150px;
+    }
+}
+
+@media screen and (max-width: 1140px) {
+
+    .th-title,
+    .td-title,
+    .th-name,
+    .td-name,
+    .th-content,
+    .td-content,
+    .th-created_at,
+    .td-created_at {
+        max-width: 120px;
+    }
+}
+
+@media screen and (max-width: 1000px) {
+
+    .th-created_at,
+    .td-created_at {
+        display: none;
+    }
+
+}
+
+@media screen and (max-width: 900px) {
+
+    .th-id,
+    .td-id {
+        display: none;
+    }
+}
+
+@media screen and (max-width: 768px) {
+
+    .th-id,
+    .td-id,
+    .th-title,
+    .td-title,
+    .th-name,
+    .td-name,
+    .th-content,
+    .td-content,
+    .th-created_at,
+    .td-created_at {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+}
+
+@media screen and (max-width: 500px) {
+
+    .th-content,
+    .td-content {
+        display: none;
+    }
+
+    .font-bold {
+        font-size: 15px;
+    }
+
+}
+
+@media screen and (max-width: 400px) {
+
+    .th-name,
+    .td-name,
+    .th-title,
+    .td-title {
+        padding-right: 5px;
+        padding-left: 5px;
+    }
+
+    .tabella {
+        margin: 0px;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Rules to fix the sidebar and right side dimensions */
 html,
 body {
-height: 100%;
+    height: 100%;
 }
 
 .min-h-screen {
-min-height: 100vh;
-display: flex;
-flex-direction: column;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
 }
 
 main {
-flex: 1;
+    flex: 1;
 }
 
 main>* {
-width: 100%;
+    width: 100%;
 }
 
 .message-body {
-display: inline-block;
-white-space: nowrap;
-overflow: hidden;
-text-overflow: ellipsis;
-max-width: 60ch;
+    display: inline-block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 60ch;
 }
 </style>
